14a3fdfc79c8ea3ab8ab20279e96a9f49c07ed6b
LORDDIMAS4IK <klochixindima8@gmail.com>
2023-04-27 14:57:54 +0300

    Samsung module 1

--- src/main/kotlin/module1 1.5.0.kt
+++ src/main/kotlin/module1 1.5.0.kt
@@ -1,3 +1,32 @@
-fun main(){
+interface Movable {
+    fun move(dx: Int, dy: Int)
+    //(метод move(dx, dy) добавляет dx к координате x, а dy к координате y).
+}
 
+internal class Point(var x: Int, var y: Int) : Movable {
+    //Point (точка с координатами x, y) и
+    override fun move(dx: Int, dy: Int) {
+        x += dx
+        y += dy
+    }
+}
+
+internal class CloudOfPoints(val points: ArrayList<Point>) : Movable {
+    // CloudOfPoints (набор точек Point)
+
+    override fun move(dx: Int, dy: Int) {
+        points[0].x+=dx
+        points[0].y+=dy
+    }
+}
+
+fun main() {
+    val p = Point(1, 1)
+    val cloud = CloudOfPoints(arrayListOf(Point(1, 1)))
+    p.move(1, 1);
+    cloud.move(2, 2)
+    val p2 = cloud.points[0]
+    println("${p.x} ${p.y}")
+    println("${p2.x} ${p2.y}")
+    println()
 }
